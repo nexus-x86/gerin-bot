09fa1417635ba0f86df25d7c514b063f3e4a4c55
NexusValiant <60083031+NexusValiant@users.noreply.github.com>
2021-04-10 21:29:30 -0400

    fix bug with warnings command

--- src/commands/moderation/warnings.js
+++ src/commands/moderation/warnings.js
@@ -23,7 +23,12 @@ module.exports = class warningsCommand extends Command {
 
     run(message , {userMention}) {
         const warnsEnmap = this.client.warnsEnmap
-
+        if (warnsEnmap.get(message.guild.id + "_" + userMention.id) == undefined) {
+            const embed = new discordjs.MessageEmbed()
+			.setColor('#FF0000')
+			.setAuthor(userMention.user.username + " has 0 warnings.", userMention.user.displayAvatarURL(), "https://discord.gg/FernczgPfd")
+			return message.channel.send(embed);
+        }
         const warnsArray = warnsEnmap.get(message.guild.id + "_" + userMention.id);
 
         const warnsStr = warnsArray.map((warn, i) => `**Warn #${i}** | *Moderator: ${warn.assigner}* | *Date: ${warn.time}*| \n ${warn.reason}`).join('\n')
